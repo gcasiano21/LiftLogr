3cf5d6358d66e4add80202979b8fae6477ef88a0
Giovanni Casiano <gcasiano21@gmail.com>
2024-05-04 20:07:31 -0400

    lifting page working

--- app/src/main/java/com/example/liftlogr/Lifting.java
+++ app/src/main/java/com/example/liftlogr/Lifting.java
@@ -2,13 +2,26 @@ package com.example.liftlogr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import java.util.ArrayList;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class Lifting extends AppCompatActivity {
-    ImageButton backButton;
+    private EditText editTextLiftName, editTextSets, editTextReps;
+    private Button buttonAddLift;
+    private LinearLayout checkListLayout;
+
+    private ArrayList<String> lifts = new ArrayList<>();
+
+
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +38,35 @@ public class Lifting extends AppCompatActivity {
                     }
                 }
         );
+        editTextLiftName = findViewById(R.id.editTextLiftName);
+        editTextSets = findViewById(R.id.editTextSets);
+        editTextReps = findViewById(R.id.editTextReps);
+        buttonAddLift = findViewById(R.id.buttonAddLift);
+        checkListLayout = findViewById(R.id.checkListLayout);
+
+        buttonAddLift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addLift();
+            }
+        });
+    }
+
+    private void addLift() {
+        String liftName = editTextLiftName.getText().toString();
+        String sets = editTextSets.getText().toString();
+        String reps = editTextReps.getText().toString();
+
+        String liftDetails = liftName + " - Sets: " + sets + ", Reps: " + reps;
+        lifts.add(liftDetails);
+
+        CheckBox checkBox = new CheckBox(this);
+        checkBox.setText(liftDetails);
+        checkListLayout.addView(checkBox);
+
+        editTextLiftName.setText("");
+        editTextSets.setText("");
+        editTextReps.setText("");
     }
 }
+
